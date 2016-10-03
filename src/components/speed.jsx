85db45c5e96e00c66b78dbb649bde035ec648bcd
Peng.Li <nowind_lee@qq.com>
2016-10-04 00:25:19 +0800

    fix typo

--- src/components/speed.jsx
+++ src/components/speed.jsx
@@ -6,7 +6,7 @@ export default class Speed extends Component {
     const {startTime, finishTime, charCount} = this.props;
     const timeInMinutes = finishTime.diff(startTime) / (1000 * 60);
     const speed = Math.floor(charCount / timeInMinutes);
-    return <div>{speed} chars per minutes</div>;
+    return <div>{speed} chars per minute</div>;
   }
 
 }
